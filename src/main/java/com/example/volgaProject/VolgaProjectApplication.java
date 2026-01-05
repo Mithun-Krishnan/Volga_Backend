71c18449c086bdaf6b1a72e96061e35f4fbabc52
Mithun Krishnan <mithunkrishnan1326@gmail.com>
2026-01-05 22:06:19 +0530

    first commit

---
+++ src/main/java/com/example/volgaProject/VolgaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.volgaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolgaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolgaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/volgaProject/VolgaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.volgaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolgaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
